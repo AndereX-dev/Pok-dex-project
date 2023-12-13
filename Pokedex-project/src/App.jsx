a95d380564f5a4605234720c636343baee17da3f
Andreas <andreaskodehode4@gmail.com>
2023-12-13 12:44:09 +0100

    commit these changes

---
+++ Pokedex-project/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>POKÉDEX</h1>
+    <p>With this app you can browse through all the different Pokémon you know, You can also get know more about your favorit Pokémon like their abilities, evolution and even more. The app is still in progress of gathering and catching more Pokémon so stay tuned.</p>
+    </>
+  )
+}
+
+export default App
